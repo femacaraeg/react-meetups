0e66a92de9596030a029c217b6033d85650c745d
Fe Macaraeg <fem@codev.com>
2022-09-23 14:03:49 +0800

    Add Meetup list on homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/5/5e/El_Nido_Palawan_2.jpg',
+    address: 'Some address, 23232, Some City, Some Country',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/5/5e/El_Nido_Palawan_2.jpg',
+    address: 'Some address, 23232, Some City, Some Country',
+    description: 'This is a second meetup!',
+  },
+];
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
