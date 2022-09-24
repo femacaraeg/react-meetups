3cf88d47c66975f74be478f09ea6ca27465cadfc
Fe Macaraeg <femacaraeg@gmail.com>
2022-09-24 14:08:01 +0800

    Add data fetching in meetup detail page.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,42 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //  fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/5/5e/El_Nido_Palawan_2.jpg',
+        title: 'A First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
 export default MeetupDetails;
