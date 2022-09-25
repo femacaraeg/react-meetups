d3d0525870926ad4d52eabd4130d73426bc06561
Fe Macaraeg <femacaraeg@gmail.com>
2022-09-25 21:21:08 +0800

    Fetch meetups on build.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MongoClient } from 'mongodb';
 
 import MeetupList from '../components/meetups/MeetupList';
 
@@ -27,11 +28,28 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://master:R8miijHH.Kc4jYe@cluster0.sor9xfy.mongodb.net/MeetupsDatabase?retryWrites=true&w=majority'
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
 
